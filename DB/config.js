bd187462bafbbcf4c4e6df587fcea74edf1ed304
aryananand27 <mgsaryananand2003@example.com>
2024-10-18 17:26:08 +0530

    Deployed

--- DB/config.js
+++ DB/config.js
@@ -1,6 +1,8 @@
 const mongoose=require("mongoose");
+require('dotenv').config();
+const DB=process.env.DATABASE;
 
-mongoose.connect('mongodb://127.0.0.1:27017/Comic').then(()=>{
+mongoose.connect(DB).then(()=>{
     console.log("connection to database done!!");
 }).catch((err)=>{
     console.log("error in connecting to local database");
